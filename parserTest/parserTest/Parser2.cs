5ff79b34f7ca3cb4b96fca007df137804444a3a8
David Gulyas <gulyman@gmail.com>
2023-01-14 22:06:40 -0700

    Start making second parser

---
+++ parserTest/parserTest/Parser2.cs
@@ -0,0 +1,115 @@
+namespace parserTest
+{
+	/// <summary>
+	/// This code is garbage, but it does parse and solve math
+	/// where allowed symbols are [0-9, +, *, (, )]
+	/// The functions are heavily dependant on the guarantees
+	/// given by the other functions and there are probably a
+	/// bunch of edge cases that break it.
+	/// </summary>
+	public static class Parser2
+	{
+		public static int Parse(string s)
+		{
+			var stack = Split(s);
+			var cur = "";
+			do
+			{
+				cur = stack[stack.Count -1] + cur;
+				stack.RemoveAt(stack.Count-1);
+
+				(var start, var end) = BreakString(cur);
+
+				cur = Calc(start).ToString() + end;
+			}while (stack.Count > 0);
+
+			return int.Parse(cur);
+		}
+
+		public static (string, string, string) TriSplit(string s, int start, int end){
+			var first = "";
+			var second = "";
+			var third = "";
+
+			if(start > 0)
+			{
+				first = s.Substring(0, start);
+			}
+
+			second = s.Substring(start + 1, end - start - 1);
+
+			if(end < s.Length)
+			{
+				third = s.Substring(end + 1, s.Length - end - 1);
+			}
+
+			return (first, second, third);
+		}
+
+		/// <summary>
+		/// If the string starts with (, break out the math inside
+		/// the first set of () and return that as start. Return
+		/// the rest of the math after the first () as end.
+		/// </summary>
+		/// <param name="cur"></param>
+		/// <returns></returns>
+		private static (string, string) BreakString(string cur)
+		{
+			var start = cur;
+			var end = "";
+
+			if (cur[0] == '(')
+			{
+				var breakIndex = cur.IndexOf(')');
+				start = cur.Substring(1, breakIndex-1);
+				end = cur.Substring(breakIndex+1, cur.Length - breakIndex-1);
+			}
+
+			return (start, end);
+		}
+
+		/// <summary>
+		/// Calculates the answer for a string of additions and multiplications
+		/// </summary>
+		/// <param name="s"></param>
+		/// <returns></returns>
+		public static int Calc(string s)
+		{
+			var sum = 0;
+			foreach (var element in s.Split('+'))
+			{
+				var prod = 1;
+				foreach (var number in element.Split('*'))
+				{
+					prod *= int.Parse(number);
+				}
+
+				sum += prod;
+			}
+			return sum;
+		}
+
+
+		/// <summary>
+		/// Equivalent to s.split( '(' ) but the ( is included in the token
+		/// </summary>
+		/// <param name="s">The string to split</param>
+		/// <returns></returns>
+		public static List<string> Split(string s, char c = '(')
+		{
+			var list = new List<string>();
+			var cur = "" + s[0];
+			for(int i = 1; i < s.Length; i++)
+			{
+				if(s[i] == c){
+					list.Add(cur);
+					cur = "";
+				}
+				cur += s[i];
+			}
+			list.Add(cur);
+
+			return list;
+		}
+	}
+}
--- parserTest/parserTest/Program.cs
+++ parserTest/parserTest/Program.cs
@@ -9,11 +9,16 @@
 			//var z = Calc("2*3+2*3+3+5*6+5"); //50
 			//var x = Split(" a+(b+c)*(d*(e+f)*(g+h)+i)", '(' );
 
+            // (var a, var b, var c) = Parser2.TriSplit("0123456789", 2,6);
+            // (var aa, var ab, var ac) = Parser2.TriSplit("0123456789", 0,6);
+            // (var ba, var bb, var bc) = Parser2.TriSplit("0123456789", 2,9);
+            // (var baa, var bab, var abc) = Parser2.TriSplit("0123456789", 0,9);
+
 			var u = Parser1.Parse("2+(4+1)*(3*(5+2)*(2+6)+10)");
 			var v =                2+(4+1)*(3*(5+2)*(2+6)+10);
 
 			Console.WriteLine(u + " == " + v);
-			Console.ReadLine();
+			//Console.ReadLine();
 		}
 
 	}
