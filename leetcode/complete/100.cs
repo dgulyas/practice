62196f7602e0ab593793a03fe5a29929003f270d
David Gulyas <gulyman@gmail.com.com>
2024-09-13 14:40:46 -0600

    Solved leetcode #100

---
+++ leetcode/complete/100.cs
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public bool IsSameTree(TreeNode p, TreeNode q) {
+        return IsSameNode(p, q);
+    }
+
+    private bool IsSameNode(TreeNode p, TreeNode q){
+        if(p == null && q == null){
+            return true;
+        }
+        if(p == null || q == null){
+            return false;
+        }
+        if(p.val != q.val){
+            return false;
+        }
+        return IsSameNode(p.left, q.left) && IsSameNode(p.right, q.right);
+    }
+}
