1d13251536b437798817ec5f77d969ba692edfdc
David Gulyas <gulyman@gmail.com.com>
2024-09-19 21:54:09 -0600

    RoutingTut: Add routing to details page

--- React/routing-tutorial/src/main.jsx
+++ React/routing-tutorial/src/main.jsx
@@ -7,13 +7,22 @@ import {
 import "./index.css";
 import Root from "./routes/root";
 import ErrorPage from "./error-page";
+import Contact from "./routes/contact";
+import { Outlet } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
   },
+
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
