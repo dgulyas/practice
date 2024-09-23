b3cfe016aca1199155bc005fca94652aa1c648da
David Gulyas <gulyman@gmail.com.com>
2024-09-23 13:53:17 -0600

    Add functionality to add new row to table

--- React/material-ui/src/App.js
+++ React/material-ui/src/App.js
@@ -16,26 +16,42 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import { useState } from 'react';
 
-function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number,
-) {
-  return { name, calories, fat, carbs, protein };
-}
 
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
+
+
+
+
 
 export default function App() {
+  const [Name, setName] = useState('')
+  const [Calories, setCalories] = useState('')
+  const [Carbs, setCarbs] = useState('')
+  const [Protein, setProtein] = useState('')
+
+  function createData(
+    name: string,
+    calories: number,
+    carbs: number,
+    protein: number,
+  ) {
+    return { name, calories, carbs, protein };
+  }
+
+  const [rows, setRows] = useState([
+    createData('Frozen yoghurt', 159, 24, 4.0),
+    createData('Ice cream sandwich', 237, 37, 4.3),
+    createData('Eclair', 262, 24, 6.0),
+    createData('Cupcake', 305, 67, 4.3),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+  ]);
+
+  function AddRow(){
+    setRows([...rows, createData(Name, Calories, Carbs, Protein)]);
+  }
+
   return (
     <div>
       <TableContainer component={Paper}>
@@ -44,7 +60,6 @@ export default function App() {
             <TableRow>
               <TableCell>Dessert (100g serving)</TableCell>
               <TableCell align="right">Calories</TableCell>
-              <TableCell align="right">Fat&nbsp;(g)</TableCell>
               <TableCell align="right">Carbs&nbsp;(g)</TableCell>
               <TableCell align="right">Protein&nbsp;(g)</TableCell>
             </TableRow>
@@ -59,7 +74,6 @@ export default function App() {
                   {row.name}
                 </TableCell>
                 <TableCell align="right">{row.calories}</TableCell>
-                <TableCell align="right">{row.fat}</TableCell>
                 <TableCell align="right">{row.carbs}</TableCell>
                 <TableCell align="right">{row.protein}</TableCell>
               </TableRow>
@@ -68,8 +82,12 @@ export default function App() {
         </Table>
       </TableContainer>
       
-      <Button variant="contained">Add New</Button>
-      
+      <Button variant="contained" onClick={() => {AddRow();}}>Add New</Button>
+      <TextField id="Name" label="Name" variant="outlined" onChange={e => {setName(e.target.value)}}/>
+      <TextField id="Calories" label="Calories" variant="outlined" onChange={e => {setCalories(e.target.value)}}/>
+      <TextField id="Carbs" label="Carbs (g)" variant="outlined" onChange={e => {setCarbs(e.target.value)}}/>
+      <TextField id="Protein" label="Protein (g)" variant="outlined" onChange={e => {setProtein(e.target.value)}}/>
     </div>
   );
 }
+
